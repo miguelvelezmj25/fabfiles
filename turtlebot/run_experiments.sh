a3efcf81e92a9d12a74b7adbfb14d684adec92b9
Miguel Velez <miguelvelez@mijecu25.com>
2016-10-21 11:32:08 -0400

    Fix script

--- turtlebot/run_experiments.sh
+++ turtlebot/run_experiments.sh
@@ -2,5 +2,5 @@
 
 password=$1
 
-fab -f ../turtlebot.py -p ${password} -I set_hosts:hosts-1 run_experiments_single_host:30,/home/mvelezce/catkin_ws/src/cp1_gazebo/instructions/localization
-fab -f ../turtlebot.py -p ${password} -I set_hosts:hosts-2 run_experiments_single_host:30,/home/mvelezce/catkin_ws/src/cp1_gazebo/instructions/localization
+fab -P -f ../turtlebot.py -p ${password} set_hosts:hosts-1 run_experiments_single_host:30,/home/mvelezce/catkin_ws/src/cp1_gazebo/instructions/localization
+fab -P -f ../turtlebot.py -p ${password} set_hosts:hosts-2 run_experiments_single_host:30,/home/mvelezce/catkin_ws/src/cp1_gazebo/instructions/localization
