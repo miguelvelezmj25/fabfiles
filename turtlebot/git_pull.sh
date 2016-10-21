0929bcde13b6140b4611cf0e237d2de12bae7965
Miguel Velez <miguelvelez@mijecu25.com>
2016-10-21 16:15:24 -0400

    Fix typo

--- turtlebot/git_pull.sh
+++ turtlebot/git_pull.sh
@@ -2,4 +2,4 @@
 
 password=$1
 
-fab -P -f ../git.py -p ${password} set_hosts:turtlebot_hosts git_checkout:'-- .',./catkin_ws git_pull:./catkin_ws git_submodule:'update --remote',./catkin_ws
+fab -P -f ../git.py -p ${password} set_hosts:turtlebot-hosts git_checkout:'-- .',./catkin_ws git_pull:./catkin_ws git_submodule:'update --remote',./catkin_ws
