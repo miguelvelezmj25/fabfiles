f9b0dca19f1f9f3f29ea7fc0286a1c7681bd1978
Miguel Velez <miguelvelez@mijecu25.com>
2016-10-21 10:47:12 -0400

    Paralellize

--- turtlebot/git_pull.sh
+++ turtlebot/git_pull.sh
@@ -1,1 +1,3 @@
-fab -P -f ../git.py -p 'mvelezce' set_hosts:turtlebot_hosts git_checkout:'-- .',./catkin_ws git_pull:./catkin_ws git_submodule:'update --remote',./catkin_ws
+#!/bin/bash
+
+fab -P -f ../git.py -P -p 'mvelezce' set_hosts:turtlebot_hosts git_checkout:'-- .',./catkin_ws git_pull:./catkin_ws git_submodule:'update --remote',./catkin_ws
