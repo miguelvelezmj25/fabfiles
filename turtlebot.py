613ffe7a406b67ee937f59d4beb376107935cbeb
Miguel Velez <miguelvelez@mijecu25.com>
2016-10-21 10:57:33 -0400

    Set and fix bash_profile

--- turtlebot.py
+++ turtlebot.py
@@ -7,10 +7,22 @@ def run_experiments_single_host(iterations, dir="."):
     execute('echo $ROS_MASTER_URI')
     dot('.bash_profile')
     execute('./run_experiments.sh ' + iterations, dir)
-    # execute('python -c "import latest_config_to_csv as m; m.save_host()"', dir)
 
 
-def fix():
+def set_bash_profile():
+    execute('echo "source $HOME/.bashrc" > $HOME/.bash_profile')
+    execute('echo " " >> $HOME/.bash_profile')
+    execute('echo "source /opt/ros/indigo/setup.bash" >> $HOME/.bash_profile')
+    execute('echo "source /home/mvelezce/catkin_ws/devel/setup.bash" >> $HOME/.bash_profile')
+    execute('echo "export ROS_IP=$(hostname -I)" >> $HOME/.bash_profile')
+    execute('echo "export DISPLAY=:1" >> $HOME/.bash_profile')
+    execute('echo " " >> $HOME/.bash_profile')
+
+
+def remove_last_line_bash_profile():
     execute("sed '$d' $HOME/.bash_profile > $HOME/.bash_profile-new")
     execute("rm $HOME/.bash_profile")
     execute("mv $HOME/.bash_profile-new $HOME/.bash_profile")
+
+
+
