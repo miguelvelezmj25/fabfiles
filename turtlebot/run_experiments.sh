d503cdf836db41e4a47615fed29f10234498e702
Miguel Velez <miguelvelez@mijecu25.com>
2016-10-29 11:54:19 -0400

    Change the host that will execute experiments

--- turtlebot/run_experiments.sh
+++ turtlebot/run_experiments.sh
@@ -2,6 +2,4 @@
 
 password=$1
 
-fab -P -f ../turtlebot.py -p ${password} set_hosts:hosts-1 run_experiments_single_host:740,/home/mvelezce/catkin_ws/src/cp1_gazebo/instructions/localization
-fab -P -f ../turtlebot.py -p ${password} set_hosts:hosts-2 run_experiments_single_host:740,/home/mvelezce/catkin_ws/src/cp1_gazebo/instructions/localization
-fab -P -f ../turtlebot.py -p ${password} set_hosts:hosts-3 run_experiments_single_host:740,/home/mvelezce/catkin_ws/src/cp1_gazebo/instructions/localization
+fab -P -f ../turtlebot.py -p ${password} set_hosts:hosts-1 run_experiments_single_host:1000,/home/mvelezce/catkin_ws/src/cp1_gazebo/instructions/localization
