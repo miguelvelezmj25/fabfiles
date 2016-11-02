9115a0c4c6689a256b272692631bb25514fcd5b6
Miguel Velez <miguelvelez@mijecu25.com>
2016-11-02 15:11:45 -0400

    Get ready for experiments

--- turtlebot/run_experiments.sh
+++ turtlebot/run_experiments.sh
@@ -2,4 +2,4 @@
 
 password=$1
 
-fab -P -f ../turtlebot.py -p ${password} set_hosts:hosts-1 run_experiments_single_host:1000,/home/mvelezce/catkin_ws/src/cp1_gazebo/instructions/localization
+fab -P -f ../turtlebot.py -p ${password} set_hosts:hosts-placeholder run_experiments_single_host:110,/home/mvelezce/catkin_ws/src/cp1_gazebo/instructions/localization
