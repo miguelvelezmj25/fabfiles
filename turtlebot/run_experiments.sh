3258633cc27a53bf560eaf6d6f2e64903572928c
Miguel Velez <miguelvelez@mijecu25.com>
2016-11-12 12:29:20 -0500

    Change variable name

--- turtlebot/run_experiments.sh
+++ turtlebot/run_experiments.sh
@@ -5,6 +5,6 @@
 
 hosts=$1
 password=$2
-executions=$3
+iterations=$3
 
-fab -P -f ../turtlebot.py -p ${password} set_hosts:${hosts} run_experiments_single_host:${executions},/home/mvelezce/catkin_ws/src/cp1_gazebo/instructions/localization
+fab -P -f ../turtlebot.py -p ${password} set_hosts:${hosts} run_experiments_single_host:${iterations},/home/mvelezce/catkin_ws/src/cp1_gazebo/instructions/localization
